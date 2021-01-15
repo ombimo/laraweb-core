ee1dcf526ce6d9151bf686c7064b7b5c0a65e3ea
Bimo Aji Pamungkas <bimo.aji.92@gmail.com>
2021-01-15 13:23:05 +0700

    persiapan untuk alert

---
+++ src/Helpers/Alert.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Ombimo\LarawebCore\Helpers;
+
+use Spatie\SchemaOrg\Schema;
+
+class Alert
+{
+    public static $type;
+
+    public static $msg;
+
+    public static function set($type, $msg)
+    {
+        self::$type = $type;
+        self::$msg = $msg;
+    }
+
+    public static function get()
+    {
+        return view('_core.alert');
+    }
+}
